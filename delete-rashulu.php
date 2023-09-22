96765481e15902dc00ec8898cf17e6a478aa1b85
AKraja777 <ashokkumar7hitter@gmail.com>
2023-09-22 17:34:38 +0530

    delete-rashulu.php

--- delete-rashulu.php
+++ delete-rashulu.php
@@ -14,6 +14,8 @@ $db->connect();
 
 	$sql_query = "DELETE  FROM rashulu_tab WHERE id =" . $ID;
 	$db->sql($sql_query);
+	$sql_query = "DELETE  FROM rashulu_tab_variant WHERE rashulu_tab_id =" . $ID;
+	$db->sql($sql_query);
 	$res = $db->getResult();
 	header("location:rashulu.php");
 ?>
