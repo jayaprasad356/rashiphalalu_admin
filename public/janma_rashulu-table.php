f196559dd7f743f0eeb5bc358299d7da83f46719
AKraja777 <ashokkumar7hitter@gmail.com>
2023-09-22 15:47:05 +0530

    janma-table.php

--- public/janma_rashulu-table.php
+++ public/janma_rashulu-table.php
@@ -47,7 +47,7 @@
     </section>
     <style>
     /* Truncate the description to show only a single line */
-    .box-body table td:nth-child(4) {
+    .box-body table td:nth-child(3) {
         max-width: 200px; /* Adjust the width to control the truncation */
         white-space: nowrap;
         overflow: hidden;
