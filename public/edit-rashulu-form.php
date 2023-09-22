68b7a5f45d94f0dda116a594d379791080ad36ce
AKraja777 <ashokkumar7hitter@gmail.com>
2023-09-22 14:59:00 +0530

    edit-rashulu-form.php

--- public/edit-rashulu-form.php
+++ public/edit-rashulu-form.php
@@ -126,7 +126,7 @@ if (isset($_POST['btnCancel'])) { ?>
 									 </div>
 									 <div class="col-md-4">
 										<label for="exampleInputEmail1">Description</label><i class="text-danger asterik">*</i>
-										<textarea type="text" row="2" class="form-control" name="description" value="<?php echo $res[0]['description']; ?>"></textarea>
+										<input type="text" rows="2" class="form-control" name="description" value="<?php echo $res[0]['description']; ?>">
 									 </div>
 								</div>
 						   </div>
