502fca5377b9216082900ee1cbeba7299811c47d
AKraja777 <ashokkumar7hitter@gmail.com>
2023-09-27 07:58:25 +0530

    delete-janma.php

---
+++ delete-janma.php
@@ -0,0 +1,21 @@
+<?php
+include_once('includes/crud.php');
+$db = new Database();
+$db->connect();
+	
+	if (isset($_GET['id'])) {
+		$ID = $db->escapeString($_GET['id']);
+	} else {
+		// $ID = "";
+		return false;
+		exit(0);
+	}
+	$data = array();
+
+	$sql_query = "DELETE  FROM janma_tab WHERE id =" . $ID;
+	$db->sql($sql_query);
+	$sql_query = "DELETE  FROM janma_tab_variant WHERE janma_tab_id =" . $ID;
+	$db->sql($sql_query);
+	$res = $db->getResult();
+	header("location:janma.php");
+?>
